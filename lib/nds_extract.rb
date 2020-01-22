9e691f1af7d480cff6f47a212c6c3c14156a51fe
Carl Parm <carlparm@gmail.com>
2020-01-22 18:21:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,6 +19,18 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
+ directors = 0
   result = {}
-  nil
+  while directors < nds.length do
+    result[nds[directors][:name]] = 0
+    movie = 0
+    total = 0
+    while movie < nds[directors][:movies].length do
+      total += nds[directors][:movies][movie][:worldwide_gross]
+      movie += 1
+    end
+    result[nds[directors][:name]] = total
+  directors += 1
+  end
+return result  
 end
